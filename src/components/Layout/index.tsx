8b02d774835a3dee43764eb4629706d02503cbc4
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-01-05 10:56:31 -0500

    SJIP-26: refactor feature toggle notification banner and add to dashboard

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Layout as AntLayout } from "antd";
 import Header from "components/Layout/Header";
 import Footer from "components/Layout/Footer";
-import SiteWideBanner from "./SiteWideBanner";
+import { getFTEnvVarByKey } from "helpers/EnvVariables";
+import NotificationBanner from "components/featureToggle/NotificationBanner";
+import { AlterTypes } from "utils/types";
 
 import styles from "./index.module.scss";
 
@@ -10,9 +12,20 @@ interface OwnProps {
   children: React.ReactElement;
 }
 
+const FT_FLAG_KEY = "SITE_WIDE_BANNER";
+const BANNER_TYPE_KEY = FT_FLAG_KEY + "_TYPE";
+const BANNER_MSG_KEY = FT_FLAG_KEY + "_MSG";
+
 const Layout = ({ children }: OwnProps) => (
   <AntLayout className={styles.mainLayout}>
-    <SiteWideBanner />
+    <NotificationBanner
+      className={styles.siteWideBanner}
+      featureToggleKey={FT_FLAG_KEY}
+      type={getFTEnvVarByKey<AlterTypes>(BANNER_TYPE_KEY, "warning")}
+      message={getFTEnvVarByKey(BANNER_MSG_KEY)}
+      banner
+      closable
+    />
     <Header />
     <AntLayout.Content className={styles.mainContent}>
       <div id="content">{children}</div>

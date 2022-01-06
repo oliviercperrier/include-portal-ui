353f259a16a6d10eb495c7d21b180cc84e9e1f26
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-01-06 16:06:24 -0500

    Make header fix top

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -17,11 +17,17 @@ import { STATIC_ROUTES } from "utils/routes";
 import history from "utils/history";
 import { useUser } from "store/user";
 import intl from "react-intl-universal";
-
-import style from "./index.module.scss";
+import { getFTEnvVarByKey } from "helpers/EnvVariables";
+import NotificationBanner from "components/featureToggle/NotificationBanner";
+import { AlterTypes } from "utils/types";
 import { useKeycloak } from "@react-keycloak/web";
 
+import style from "./index.module.scss";
+
 const iconSize = { width: 14, height: 14 };
+const FT_FLAG_KEY = "SITE_WIDE_BANNER";
+const BANNER_TYPE_KEY = FT_FLAG_KEY + "_TYPE";
+const BANNER_MSG_KEY = FT_FLAG_KEY + "_MSG";
 
 const Header = () => {
   const { user } = useUser();
@@ -35,89 +41,99 @@ const Header = () => {
   };
 
   return (
-    <PageHeader
-      title={<IncludeIcon className={style.logo} />}
-      subTitle={
-        <nav className={style.headerList}>
-          <HeaderLink
-            currentPathName={currentPathName}
-            to={STATIC_ROUTES.DASHBOARD}
-            icon={<HomeOutlined />}
-            title={intl.get("layout.main.menu.dashboard")}
-          />
-          <HeaderLink
-            currentPathName={currentPathName}
-            to={STATIC_ROUTES.STUDIES}
-            icon={<ReadOutlined />}
-            title={intl.get("layout.main.menu.studies")}
-          />
-          <HeaderLink
-            currentPathName={currentPathName}
-            to={STATIC_ROUTES.DATA_EXPLORATION}
-            icon={<FileSearchOutlined />}
-            title={intl.get("layout.main.menu.explore")}
-          />
-          <HeaderLink
-            currentPathName={currentPathName}
-            to={STATIC_ROUTES.DATA_EXPLORATION_PARTICIPANTS}
-            icon={<UserOutlined />}
-            title={intl.get("layout.main.menu.participants")}
-          />
-          <HeaderLink
-            currentPathName={currentPathName}
-            to={STATIC_ROUTES.DATA_EXPLORATION_BIOSPECIMENS}
-            icon={<ExperimentOutlined />}
-            title={intl.get("layout.main.menu.biospecimen")}
-          />
-          <HeaderLink
-            currentPathName={currentPathName}
-            to={STATIC_ROUTES.DATA_EXPLORATION_DATAFILES}
-            icon={<FileTextOutlined />}
-            title={intl.get("layout.main.menu.datafiles")}
-          />
-        </nav>
-      }
-      extra={[
-        <Button key="external-website" className={style.headerBtn}>
-          {intl.get("layout.main.menu.website")}{" "}
-          <ExternalLinkIcon className={style.icon} {...iconSize} />
-        </Button>,
-        <Dropdown
-          key="user-menu"
-          trigger={["click"]}
-          overlay={
-            <Menu>
-              <Menu.Item key="profile">
-                {intl.get("layout.user.menu.myprofile")}
-              </Menu.Item>
-              <Menu.Item key="settings">
-                {intl.get("layout.user.menu.settings")}
-              </Menu.Item>
-              <Menu.Divider key="divider 1" />
-              <Menu.Item key="logout" onClick={handleLogout}>
-                {intl.get("layout.user.menu.logout")}
-              </Menu.Item>
-            </Menu>
-          }
-        >
-          <a
-            className={style.userMenuTrigger}
-            onClick={(e) => e.preventDefault()}
-            href=""
-          >
-            {/* CHANGE WITH USER */}
-            <Gravatar
-              className={style.userGravatar}
-              email="ocastro-perrier@ferlab.bio"
-              size={100}
+    <>
+      <NotificationBanner
+        className={style.siteWideBanner}
+        featureToggleKey={FT_FLAG_KEY}
+        type={getFTEnvVarByKey<AlterTypes>(BANNER_TYPE_KEY, "warning")}
+        message={getFTEnvVarByKey(BANNER_MSG_KEY)}
+        banner
+        closable
+      />
+      <PageHeader
+        title={<IncludeIcon className={style.logo} />}
+        subTitle={
+          <nav className={style.headerList}>
+            <HeaderLink
+              currentPathName={currentPathName}
+              to={STATIC_ROUTES.DASHBOARD}
+              icon={<HomeOutlined />}
+              title={intl.get("layout.main.menu.dashboard")}
             />
-            <span className={style.userName}>{user?.firstName}</span>
-            <DownOutlined />
-          </a>
-        </Dropdown>,
-      ]}
-      className={style.mainHeader}
-    />
+            <HeaderLink
+              currentPathName={currentPathName}
+              to={STATIC_ROUTES.STUDIES}
+              icon={<ReadOutlined />}
+              title={intl.get("layout.main.menu.studies")}
+            />
+            <HeaderLink
+              currentPathName={currentPathName}
+              to={STATIC_ROUTES.DATA_EXPLORATION}
+              icon={<FileSearchOutlined />}
+              title={intl.get("layout.main.menu.explore")}
+            />
+            <HeaderLink
+              currentPathName={currentPathName}
+              to={STATIC_ROUTES.DATA_EXPLORATION_PARTICIPANTS}
+              icon={<UserOutlined />}
+              title={intl.get("layout.main.menu.participants")}
+            />
+            <HeaderLink
+              currentPathName={currentPathName}
+              to={STATIC_ROUTES.DATA_EXPLORATION_BIOSPECIMENS}
+              icon={<ExperimentOutlined />}
+              title={intl.get("layout.main.menu.biospecimen")}
+            />
+            <HeaderLink
+              currentPathName={currentPathName}
+              to={STATIC_ROUTES.DATA_EXPLORATION_DATAFILES}
+              icon={<FileTextOutlined />}
+              title={intl.get("layout.main.menu.datafiles")}
+            />
+          </nav>
+        }
+        extra={[
+          <Button key="external-website" className={style.headerBtn}>
+            {intl.get("layout.main.menu.website")}{" "}
+            <ExternalLinkIcon className={style.icon} {...iconSize} />
+          </Button>,
+          <Dropdown
+            key="user-menu"
+            trigger={["click"]}
+            overlay={
+              <Menu>
+                <Menu.Item key="profile">
+                  {intl.get("layout.user.menu.myprofile")}
+                </Menu.Item>
+                <Menu.Item key="settings">
+                  {intl.get("layout.user.menu.settings")}
+                </Menu.Item>
+                <Menu.Divider key="divider 1" />
+                <Menu.Item key="logout" onClick={handleLogout}>
+                  {intl.get("layout.user.menu.logout")}
+                </Menu.Item>
+              </Menu>
+            }
+          >
+            <a
+              className={style.userMenuTrigger}
+              onClick={(e) => e.preventDefault()}
+              href=""
+            >
+              {/* CHANGE WITH USER */}
+              <Gravatar
+                className={style.userGravatar}
+                email="ocastro-perrier@ferlab.bio"
+                size={100}
+              />
+              <span className={style.userName}>{user?.firstName}</span>
+              <DownOutlined />
+            </a>
+          </Dropdown>,
+        ]}
+        className={style.mainHeader}
+      />
+    </>
   );
 };
 

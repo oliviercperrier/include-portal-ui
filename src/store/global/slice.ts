f1dcdfdc10015c9ca1a5730d55b7ea3519c7b62d
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-03-01 11:32:19 -0500

    Centralize notification and messages

--- src/store/global/slice.ts
+++ src/store/global/slice.ts
@@ -1,15 +1,20 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { initialState } from "store/global/types";
-import { LANG } from "common/constants";
-import intl from "react-intl-universal";
-import locales from "locales";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { initialState } from 'store/global/types';
+import { LANG } from 'common/constants';
+import intl from 'react-intl-universal';
+import locales from 'locales';
+import { ArgsProps as NotificationArgsProps } from 'antd/lib/notification';
+import { ArgsProps as MessageArgsProps } from 'antd/lib/message';
 
 export const GlobalState: initialState = {
   lang: LANG.EN,
+  notification: undefined,
+  message: undefined,
+  messagesToDestroy: [],
 };
 
 const globalSlice = createSlice({
-  name: "global",
+  name: 'global',
   initialState: GlobalState,
   reducers: {
     changeLang: (state, action: PayloadAction<LANG>) => {
@@ -21,8 +26,29 @@ const globalSlice = createSlice({
       return {
         ...state,
         lang: action.payload,
-      }
+      };
     },
+
+    displayMessage: (state, action: PayloadAction<MessageArgsProps>) => ({
+      ...state,
+      message: action.payload,
+    }),
+
+    destroyMessages: (state, action: PayloadAction<string[]>) => ({
+      ...state,
+      message: undefined,
+      messagesToDestroy: action.payload,
+    }),
+
+    displayNotification: (state, action: PayloadAction<NotificationArgsProps>) => ({
+      ...state,
+      notification: action.payload,
+    }),
+
+    destroyNotification: (state) => ({
+      ...state,
+      notification: undefined,
+    }),
   },
 });
 

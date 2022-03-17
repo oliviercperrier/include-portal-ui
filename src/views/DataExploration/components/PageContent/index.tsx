2affe9b92869ac784227c2fa11f64d25ea6da769
adrian <adipaul1981@yahoo.ca>
2022-03-17 15:22:51 -0400

    fix out of range

--- src/views/DataExploration/components/PageContent/index.tsx
+++ src/views/DataExploration/components/PageContent/index.tsx
@@ -119,7 +119,7 @@ const PageContent = ({
       index: DEFAULT_PAGE_INDEX,
     });
     // eslint-disable-next-line
-  }, [filters.id]);
+  }, [filters.id, filters.content.length]);
 
   const facetTransResolver = (key: string) => {
     const title = intl.get(`facets.${key}`);

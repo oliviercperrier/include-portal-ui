0d457863a72652afaa90930737f773cb891f4e45
Olivier Castro-Perrier <olivier.cperrier@hotmail.com>
2022-06-20 10:46:27 -0400

    Small fix ids for custom facets

--- src/store/savedSet/index.ts
+++ src/store/savedSet/index.ts
@@ -7,7 +7,7 @@ export type { initialState as SavedSetInitialState } from './types';
 export { default, SavedSetState } from './slice';
 
 export const MAX_LENGTH_NAME = 50;
-export const getSetFieldId = (type: SetType) => `${type}_fhir_id`;
+export const getSetFieldId = (type: SetType) => `${type}_facet_ids.${type}_fhir_id_1`;
 export const PROJECT_ID = EnvironmentVariables.configFor('ARRANGER_PROJECT_ID');
 
 export const useSavedSet = () => useSelector(savedSetSelector);
